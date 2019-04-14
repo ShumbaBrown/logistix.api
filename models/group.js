513d7952138d50ee5f7028db5da3301edc0e85d9
Kode Williams <kode.a.williams@gmail.com>
2019-04-13 23:11:10 -0700

    Added other fields to models and controllers

--- models/group.js
+++ models/group.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose')
 
 var groupSchema = mongoose.Schema({
   groupId: String,
-  users: Array
+  users: Array,
+  bills: Array,
 })
 
 module.exports = mongoose.model('Group', groupSchema)
